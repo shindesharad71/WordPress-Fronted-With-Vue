e8b3f8ab8d59e67d48779c5859c0bf1f10c56439
Sharad Shinde <shindesharad71@gmail.com>
2017-08-18 01:38:59 +0530

    footer added

---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer>
+      <div class="text-center">By Sharad Shinde 2017</div>
+    </footer>
+</template>
+
+<script>
+export default {
+  name: 'footer'
+}
+</script>
+
+<style scoped>
+footer {
+  background: #343A40;
+  padding: 11px;
+  color: #fff;
+  margin-top: 70px;
+}
+</style>
