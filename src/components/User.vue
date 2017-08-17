3b2ee63b18b2486b67ae012a472d3c5e03958849
Sharad Shinde <shindesharad71@gmail.com>
2017-08-18 03:00:26 +0530

    author details displayed

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,3 +1,38 @@
 <template>
-  <div>User</div>
+  <div>
+    <div class="container">
+        <div class="loader text-center" v-show="loading">
+            <h3><i class="fa fa-3x fa-spinner fa-spin"></i> Loading...</h3>
+        </div>
+	</div>
+    <div class="container">
+        <img :src="user.avatar_urls[96]" class="img-fluid">
+        <h1>{{ user.name }}</h1>
+        <p>{{ user.url }}</p>
+        <p>{{ user.description }}</p>
+    </div>
+  </div>
 </template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data () {
+    return {
+      loading: true,
+      user: []
+    }
+  },
+  created() {
+		axios.get('http://sharadshinde.in/client/wp-json/wp/v2/users/'+this.$route.params.id)
+		.then((response) => {
+			this.user = response.data;
+			this.loading = false;
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+  }
+}
+</script>
